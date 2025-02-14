5a453cfa512da627e678e208055f5ef4ab991d27
Nikolay Tverdokhlebov <nn.tverd@gmail.com>
2025-02-15 00:15:53 +0500

    feat: get total insights

--- src/pages/Accounts.tsx
+++ src/pages/Accounts.tsx
@@ -15,6 +15,7 @@ export const Accounts = () => {
     const [openModal, setOpenModal] = useState(false);
     const {add} = useToaster();
     const {isProd} = useContext(AppEnvContext);
+    const [insights, setInsights] = useState<unknown>({});
 
     const handleLoadClick = useCallback(async () => {
         const json = await fetchGet({
@@ -30,6 +31,40 @@ export const Accounts = () => {
         });
     }, [add, isProd]);
 
+    const handleGetInsights = async () => {
+        const totalReach: Record<string, number> = {};
+        const totalImpressions: Record<string, number> = {};
+
+        for (const account of accounts) {
+            const {data} = await fetchGet({
+                route: ProjectRoutes.getInsights,
+                query: {id: account.id},
+                isProd,
+            });
+
+            if (!data) {
+                return;
+            }
+
+            for (const parameter of data) {
+                const {name, values} = parameter;
+                for (const val of values) {
+                    const {end_time: endTime, value} = val;
+                    if (name === 'impressions') {
+                        totalImpressions[endTime] = (totalImpressions[endTime] || 0) + value;
+                    }
+                    if (name === 'reach') {
+                        totalReach[endTime] = (totalImpressions[endTime] || 0) + value;
+                    }
+                }
+            }
+        }
+
+        // eslint-disable-next-line no-console
+        console.log({totalReach, totalImpressions});
+        setInsights({totalReach, totalImpressions});
+    };
+
     return (
         <div>
             <h2>Accounts</h2>
@@ -39,6 +74,10 @@ export const Accounts = () => {
             <Button view="outlined-action" onClick={() => setOpenModal(true)}>
                 add
             </Button>
+            <Button view="outlined-action" onClick={handleGetInsights}>
+                get insights
+            </Button>
+            <pre>{JSON.stringify(insights, null, 3)}</pre>
             {accounts.map((account) => {
                 return <Account key={account.id} {...account} />;
             })}
