eae4e2a3c73804c7f50d3fcb8ec74d35384de342
Nikolay Tverdokhlebov <nn.tverd@gmail.com>
2025-04-01 13:01:54 +0500

    feat: registration form styles

--- src/components/auth/Registration.tsx
+++ src/components/auth/Registration.tsx
@@ -1,9 +1,12 @@
 import React, {useState} from 'react';
 
+import {Button, TextInput} from '@gravity-ui/uikit';
 import {Link, Navigate, useNavigate} from 'react-router-dom';
 
 import {doCreateUserWithEmailAndPassword} from '../../auth/utils';
 import {useAuth} from '../../contexts/AuthContext';
+
+import cl from './Auth.module.css';
 
 export const Registration = () => {
     const _navigate = useNavigate();
@@ -29,84 +32,70 @@ export const Registration = () => {
             <Navigate to="#registration" />
             {userLoggedIn && <Navigate to={'/'} replace={true} />}
 
-            <main className="w-full h-screen flex self-center place-content-center place-items-center">
-                <div className="w-96 text-gray-600 space-y-5 p-4 shadow-xl border rounded-xl">
-                    <div className="text-center mb-6">
-                        <div className="mt-2">
-                            <h3 className="text-gray-800 text-xl font-semibold sm:text-2xl">
-                                Create a New Account
-                            </h3>
-                        </div>
-                    </div>
-                    <form onSubmit={onSubmit} className="space-y-4">
-                        <div>
-                            <label className="text-sm text-gray-600 font-bold">Email</label>
-                            <input
-                                type="email"
-                                autoComplete="email"
-                                required
-                                value={email}
-                                onChange={(e) => {
-                                    setEmail(e.target.value);
-                                }}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:indigo-600 shadow-sm rounded-lg transition duration-300"
-                            />
-                        </div>
-
-                        <div>
-                            <label className="text-sm text-gray-600 font-bold">Password</label>
-                            <input
-                                disabled={isRegistering}
-                                type="password"
-                                autoComplete="new-password"
-                                required
-                                value={password}
-                                onChange={(e) => {
-                                    setPassword(e.target.value);
-                                }}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
-                            />
-                        </div>
-
-                        <div>
-                            <label className="text-sm text-gray-600 font-bold">
-                                Confirm Password
-                            </label>
-                            <input
-                                disabled={isRegistering}
-                                type="password"
-                                autoComplete="off"
-                                required
-                                value={confirmPassword}
-                                onChange={(e) => {
-                                    setconfirmPassword(e.target.value);
-                                }}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
-                            />
-                        </div>
-
-                        {errorMessage && (
-                            <span className="text-red-600 font-bold">{errorMessage}</span>
-                        )}
-
-                        <button
-                            type="submit"
+            <main className={cl.main}>
+                <h3 className={cl.title}>Create a New Account</h3>
+                <form onSubmit={onSubmit} className="space-y-4">
+                    <div className={cl.inputs}>
+                        <TextInput
                             disabled={isRegistering}
-                            className={`w-full px-4 py-2 text-white font-medium rounded-lg ${isRegistering ? 'bg-gray-300 cursor-not-allowed' : 'bg-indigo-600 hover:bg-indigo-700 hover:shadow-xl transition duration-300'}`}
+                            size="xl"
+                            label="Email"
+                            value={email}
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                            }}
+                            autoComplete="email"
+                            type="email"
+                            hasClear
+                        />
+
+                        <TextInput
+                            disabled={isRegistering}
+                            size="xl"
+                            label="Password"
+                            value={password}
+                            onChange={(e) => {
+                                setPassword(e.target.value);
+                            }}
+                            autoComplete="new-password"
+                            type="password"
+                            hasClear
+                        />
+
+                        <TextInput
+                            disabled={isRegistering}
+                            size="xl"
+                            label="Confirm Password"
+                            value={confirmPassword}
+                            onChange={(e) => {
+                                setconfirmPassword(e.target.value);
+                            }}
+                            autoComplete="off"
+                            type="password"
+                            hasClear
+                        />
+                    </div>
+
+                    {errorMessage && <span className="text-red-600 font-bold">{errorMessage}</span>}
+                    <Button
+                        type="submit"
+                        disabled={isRegistering}
+                        view="action"
+                        className={cl.submit}
+                        size="xl"
+                    >
+                        {isRegistering ? 'Signing Up...' : 'Sign Up'}
+                    </Button>
+                    <div className="text-sm text-center">
+                        Already have an account? {'   '}
+                        <Link
+                            to={'/auth#login'}
+                            className="text-center text-sm hover:underline font-bold"
                         >
-                            {isRegistering ? 'Signing Up...' : 'Sign Up'}
-                        </button>
-                        <div className="text-sm text-center">
-                            Already have an account? {'   '}
-                            <Link
-                                to={'/auth#login'}
-                                className="text-center text-sm hover:underline font-bold"
-                            >
-                                Continue
-                            </Link>
-                        </div>
-                    </form>
-                </div>
+                            Continue
+                        </Link>
+                    </div>
+                </form>
             </main>
         </>
     );
