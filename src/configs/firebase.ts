0f22abf1eb3849d609afd583daab96bbf440c1d1
Nikolay Tverdokhlebov <nn.tverd@gmail.com>
2025-09-07 10:54:31 +0500

    fix: add console logs for debugging firebase configuration

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -7,11 +7,13 @@ const firebaseConfigData =
         ? import.meta.env.VITE_FIREBASE_CONFIG_PREPROD
         : import.meta.env.VITE_FIREBASE_CONFIG;
 
+console.log('firebaseConfigData', firebaseConfigData.slice(0, 30));
 if (!firebaseConfigData) {
     throw new Error('Firebase configuration not found in environment variables');
 }
 
 const firebaseConfig = JSON.parse(firebaseConfigData);
+console.log('firebaseConfig', JSON.stringify(firebaseConfig).slice(0, 30));
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseApp);
